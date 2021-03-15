c78068f3aa94aa7159989047e020c19f48ce3427
Gabriel Henrique <gabrielhcf01@gmail.com>
2021-03-15 11:31:34 -0300

    SistemaDeLogin

--- includes/message.php
+++ includes/message.php
@@ -1,15 +1,20 @@
 <?php
-    // Verificar se existe a session mensagem
-    session_start();
-    if (isset($_SESSION['mensagem'])) :
+
+
+
+// Verificar se existe a session mensagem
+
+if (isset($_SESSION['mensagem'])) :
 ?>
-<script>
-    window.onload = function() {
-        M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'})
-    };
-</script>
+    <script>
+        window.onload = function() {
+            M.toast({
+                html: '<?php echo $_SESSION['mensagem']; ?>'
+            })
+        };
+    </script>
 
 <?php
-    endif;
-    session_unset();
+endif;
+session_unset();
 ?>
